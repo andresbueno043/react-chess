de67d561c53e16aae835d12910c8d39f85d09b44
andresbueno043 <ajbueno@iegabo.edu.co>
2023-06-02 10:35:40 -0500

    Piece movement functions added

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Piece } from '@/@types';
 import Pieces from '@/@types/pieces';
 import Tile from '@/components/Tile';
@@ -6,17 +7,6 @@ const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
 const pieces: Piece[] = [];
-
-function grabPiece(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-  const element = e.target as HTMLElement;
-  if (element.classList.contains('piece')) {
-    const x = e.clientX - 37.5;
-    const y = e.clientY - 37.5;
-    element.style.position = 'absolute';
-    element.style.left = `${x}px`;
-    element.style.top = `${y}px`;
-  }
-}
 
 for (let p = 0; p < 2; p += 1) {
   const type = p === 0 ? 'black' : 'white';
@@ -48,6 +38,36 @@ for (let i = 0; i < 8; i += 1) {
 }
 
 function ChessBoard() {
+  const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
+
+  function grabPiece(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const element = e.target as HTMLElement;
+    if (element.classList.contains('piece')) {
+      const x = e.clientX - 37.5;
+      const y = e.clientY - 37.5;
+      element.style.position = 'absolute';
+      element.style.left = `${x}px`;
+      element.style.top = `${y}px`;
+      setActivePiece(element);
+    }
+  }
+
+  function movePiece(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if (activePiece) {
+      const x = e.clientX - 37.5;
+      const y = e.clientY - 37.5;
+      activePiece.style.position = 'absolute';
+      activePiece.style.left = `${x}px`;
+      activePiece.style.top = `${y}px`;
+    }
+  }
+
+  function dropPiece(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if (activePiece) {
+      setActivePiece(null);
+    }
+  }
+
   const board = [];
 
   for (let j = verticalAxis.length - 1; j >= 0; j -= 1) {
@@ -63,11 +83,14 @@ function ChessBoard() {
       board.push(<Tile key={`${i}${j}`} i={i} j={j} image={image} />);
     }
   }
+
   return (
     <div
       id="chessboard"
       className="grid grid-cols-8 grid-rows-[8] bg-[#779556] w-[600px] h-[600px]"
       onMouseDown={(e) => grabPiece(e)}
+      onMouseMove={(e) => movePiece(e)}
+      onMouseUp={(e) => dropPiece(e)}
     >
       {board}
     </div>
